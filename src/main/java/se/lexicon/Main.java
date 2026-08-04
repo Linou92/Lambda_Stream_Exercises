fd85d05fa65e5488700df95fbe3e82c3c20dea90
Lina Abu Hijleh <lina_abuhijleh@hotmail.com>
2026-08-04 15:28:26 +0200

    Write rules (lambdas) for: active, adult (age 18 or above), and lives in Stockholm

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -1,13 +1,14 @@
 package se.lexicon;
 
 import se.lexicon.lambda.Person;
+import se.lexicon.lambda.PersonRule;
 
 import java.util.List;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static void main() {
+    public static void main(String[] args) {
 
         List<Person> people = List.of(
                 new Person("Amina", 22, "Stockholm", true),
@@ -18,5 +19,16 @@ public class Main {
                 new Person("Omar", 19, "Stockholm", true)
         );
 
+        IO.println("\n --- All Persons ---");
+
+        people.forEach(person -> IO.println("Person : " + person));
+
+        // Rules (lambdas) for: active, adult (age 18 or above), and lives in Stockholm
+        PersonRule isActive = person -> person.isActive();
+        PersonRule age = person -> person.getAge() >= 18;
+        PersonRule city = person -> person.getCity().equals("Stockholm");
+
     }
+
+
 }
